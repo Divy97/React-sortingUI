fa96b73527e08fd61db0d99f35253e4e2434cef8
Divy <divyparekh1810@gmail.com>
2022-07-13 21:38:23 +0530

    Starting a Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      Hey
+    </>
+  );
+};
+
+export default App;
